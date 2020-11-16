d20065d7e2375c9faf8116c2df87f9f7b1a567aa
ste163 <samtedwards@pm.me>
2020-11-16 14:16:26 -0500

    added secret cheat mode and code comments

--- Program.cs
+++ Program.cs
@@ -12,8 +12,8 @@ namespace _05_guessing_game
             int totalTries = 0;
             int difficultyMode = 0;
 
-
-            while (difficultyMode != 1 && difficultyMode != 2 && difficultyMode != 3)
+            // While one of the difficulties is not selected, you must select one.
+            while (difficultyMode != 1 && difficultyMode != 2 && difficultyMode != 3 && difficultyMode != 4)
             {
                 Console.WriteLine("Enter a difficulty number:");
                 Console.WriteLine("1) Easy - eight guesses");
@@ -22,6 +22,7 @@ namespace _05_guessing_game
 
                 difficultyMode = int.Parse(Console.ReadLine());
 
+                // Switch for what difficulty mode was selected.
                 switch (difficultyMode)
                 {
                     case 1:
@@ -36,15 +37,26 @@ namespace _05_guessing_game
                         userTries = 4;
                         totalTries = 4;
                         break;
+                    case 4:
+                        userTries = int.MaxValue;
+                        totalTries = int.MaxValue;
+                        break; 
                 }
             }
 
+            // Tell user how many tries they have.
             Console.WriteLine($"You have {totalTries} tries.");
-            Console.WriteLine($"Guess the secret number: {secretNumber}");
+            Console.WriteLine($"Guess the secret number:");
+            // Allow user to enter int
             userGuess = int.Parse(Console.ReadLine());
+
+            // While the user has not guessed the secret number,
+            // and while they have anything other than 1 try left,
+            // allow them to guess.
             while (userGuess != secretNumber && userTries != 1)
             {
                 Console.WriteLine("Incorrect.");
+                // Give user a hint on if they guess too low or high.
                 if (userGuess < secretNumber)
                 {
                     Console.WriteLine("");
@@ -55,21 +67,25 @@ namespace _05_guessing_game
                     Console.WriteLine("");
                     Console.WriteLine("Guess is too HIGH.");
                 }
+                // Remove one of the user's tries.
                 userTries--;
+                // Tell user how many tries are left.
                 Console.WriteLine($"{userTries} out of {totalTries} tries left. Enter a new number:");
+                // Let the user guess again.
                 userGuess = int.Parse(Console.ReadLine());
                 Console.WriteLine("");
             }
 
+            // If the user has turns left, and they guess the secret number, they won!
             if (userTries != 1 || userGuess == secretNumber)
             {
                 Console.WriteLine($"Correct! The secret number is {secretNumber}");
             }
+            // If the user runs out of tries, they lose!
             else
             {
                 Console.WriteLine($"Ran out of tries!");
             }
-        
         }
     }
 }
