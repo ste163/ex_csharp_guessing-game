0d972755708f71060bdd07bb932545ec5ad4e93c
ste163 <samtedwards@pm.me>
2020-11-16 12:21:09 -0500

    refactored countdown to decrement instead of increment

--- Program.cs
+++ Program.cs
@@ -7,27 +7,37 @@ namespace _05_guessing_game
         static void Main(string[] args)
         {
             int secretNumber = new Random().Next(1, 101);
-            Console.WriteLine("Guess the secret number:");
+            Console.WriteLine($"Guess the secret number: {secretNumber}");
             int userGuess = int.Parse(Console.ReadLine());
-            int userTries = 0;
+            int userTries = 4;
 
-            while (userGuess != secretNumber && userTries < 3)
+            while (userGuess != secretNumber && userTries != 1)
             {
                 Console.WriteLine($"Incorrect.");
-                userTries++;
+                if (userGuess < secretNumber)
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Guess is too LOW.");
+                }
+                else
+                {
+                    Console.WriteLine("");
+                    Console.WriteLine("Guess is too HIGH.");
+                }
+                userTries--;
                 Console.WriteLine($"{userTries} out of 4 tries left. Enter a new number:");
                 Console.WriteLine("");
                 userGuess = int.Parse(Console.ReadLine());
                 Console.WriteLine("");
             }
 
-            if (userTries < 3 || userGuess == secretNumber)
+            if (userTries != 1 || userGuess == secretNumber)
             {
                 Console.WriteLine($"Correct! The secret number is {secretNumber}");
             }
             else
             {
-                Console.WriteLine("Ran out of tries!");
+                Console.WriteLine($"Ran out of tries!");
             }
 
         }
