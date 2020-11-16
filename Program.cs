1329d9ab4f67c182f36a74efe7d0747d742a2a1e
ste163 <samtedwards@pm.me>
2020-11-16 11:30:46 -0500

    user has 4 tries to guess correct answer

--- Program.cs
+++ Program.cs
@@ -9,12 +9,23 @@ namespace _05_guessing_game
             int secretNumber = 43;
             Console.WriteLine("Guess the secret number:");
             int userGuess = int.Parse(Console.ReadLine());
-            while (userGuess != secretNumber)
+            int userTries = 1;
+            while (userGuess != secretNumber && userTries < 4)
             {
                 Console.WriteLine($"Incorrect! You guessed {userGuess}. Try again!");
                 userGuess = int.Parse(Console.ReadLine());
+                userTries++;
             }
-            Console.WriteLine($"Correct! The secret number is {secretNumber}");
+
+            if (userTries < 4)
+            {
+                Console.WriteLine($"Correct! The secret number is {secretNumber}");
+            }
+            else
+            {
+                Console.WriteLine("Ran out of tries!");
+            }
+
         }
     }
 }
